71ea1755720e28a4e862c8b153053820c6a19933
Moh. Aflah Azzaky <aflahazzaki123@gmail.com>
2020-11-10 18:03:45 +0700

    update push.js

--- js/push.js
+++ js/push.js
@@ -12,10 +12,10 @@ webPush.setVapidDetails(
     vapidKeys.privateKey
 )
 let pushSubscription = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/ePHcMV7eF08:APA91bEhFP2hWkvuKTj9Y51vgOPZtGqnB6F6mF213r34UvH2aZMh2vfHc6MTzwm6smi8Drx_MkQf1WMRXKIrQ1mBSTL0n62D27EIaPVXgi1IA9vyp8b0cxUaB0LL-EveBezitDsS03Qf",
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cRVOOQ5Wg_o:APA91bHVn8AlP8JWtySmrL2-TFd30SwrZ-TxhSykfcKB6QVqIPIqITfTVOjEeWo0I2oKTXEmsmZTHpH3G-wVyxKoegSZM078MA_Dmo6PcIKaKfXbdA4dN1op35xGr9qRuVQoz9Kev3Kt",
     "keys": {
-        "p256dh": "BO++k/9B46BGMMoB0f8dMm+ntSvlyI7fKPPWvL4Xm7aX4iUZJUezviawFJI6HJfYkwZTaRJfFsKIEcQ01QMgMDg=",
-        "auth": "SqT6cUkJZ1JCMEcH2ubiMA=="
+        "p256dh": "BLa5NwLxPidqAuqFMP1nj2rbQp0aOuusoaGUjjugWC+i7B/FjXKterxFkyXdOH/suVjI5T7XRPc31NzIHuSDX2A=",
+        "auth": "yIJGw96A36YFTUvZK6Zt1Q=="
     }
 };
 let payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
